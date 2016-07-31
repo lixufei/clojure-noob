6a8a38b9b637fbd91b2102622973c3b954aa7000
LiXuFei <xfli@thoughtworks.com>
2016-07-31 15:18:03 +0800

    fix test.

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,3 +1,14 @@
+(ns clojure-noob.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]))
+
+  (defn app-routes []
+     (get "/" [] "<h1>Hello World</h1>")
+     (get "/foo" [] "<h1>Hello foo World</h1>")
+     (route/not-found [] "<h1>routs not found</h1>")
+        )
+
+
 ;(ns clojure-noob.core
 ;  (:gen-class))
 ;
@@ -5,15 +16,3 @@
 ;  "I don't do a whole lot ... yet."
 ;  [& args]
 ;  (println "Hello, World!"))
-
-
-(ns clojure-noob.core
-  (:require [[compojure.core :refer :all]
-             [compojure.route :as route]
-             ])
-
-  (defn app-routes
-     (get "/" [] "<h1>Hello World</h1>")
-     (get "/foo" [] "<h1>Hello foo World</h1>")
-     (route/not-found [] "<h1>routs not found</h1>")
-        ))
